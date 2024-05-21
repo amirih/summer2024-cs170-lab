8391d0754585ffac23fc5944b57dade6ae2aca1d
amirih <hossein.amiri.enterprise@gmail.com>
2024-05-20 21:21:14 -0400

    initial commit and lab01

---
+++ Lab_01/InClassWork.java
@@ -0,0 +1,31 @@
+package Lab_01;
+
+import libs.Turtle;
+/*
+
+discussion notes/ group contributions go here
+
+*/
+
+public class InClassWork {
+
+	public static void TGIF(Turtle t) {
+		// your code goes here
+
+	}
+
+	public static void stars(Turtle t) {
+		// your code goes here (remember - use a nested for loop!)
+
+	}
+
+	public static void main(String[] args) {
+		Turtle t = new Turtle();
+
+		TGIF(t);
+
+		// code to reposition your turtle goes here
+
+		stars(t);
+	}
+}
