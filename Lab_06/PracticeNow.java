9a21254513add21bdc5e21d956ac55ccb1be86b3
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2024-06-04 13:42:59 -0400

    Create PracticeNow.java

---
+++ Lab_06/PracticeNow.java
@@ -0,0 +1,38 @@
+package Lab_06;
+// Step08PracticeNowExampleSolution.java is an example solution to the problem.
+// Please study the solution and bring your own solution to the problem
+public class PracticeNow {
+    public static boolean isPalindrome(String str) {
+        return true;
+    }
+
+    public static String reverseWord(String word) {
+        String output = "";
+        return output;
+    }
+
+    public static String reverseString1(String str) {
+        String output = "";
+        return output;
+    }
+
+    public static String reverseString2(String str) {
+
+        String output = "";
+        return output;
+    }
+
+    public static String reverseString3(String str) {
+
+        String output = "";
+        return output;
+    }
+
+    public static void main(String[] args) {
+        isPalindrome("bab");
+        reverseString1("A black horse ran");
+        reverseString2("A black horse ran");
+        reverseString3("A black horse ran");
+
+    }
+}
